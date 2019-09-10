fd7095e51d8cd6c4b17db0b029eb5445ceac79bc
huhao666 <782044678@qq.com>
2019-09-10 16:51:54 +0800

    :sparkles: add Vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <HelloWorld :msg="count" @click="incrementStep()"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { mapActions, mapState } from "vuex"; //注册 action 和 state
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  computed: {
+    //在这里映射 store.state.count，使用方法和 computed 里的其他属性一样
+    ...mapState(["count"])
+  },
+  created() {
+    this.incrementStep();
+  },
+  methods: {
+    //在这里引入 action 里的方法，使用方法和 methods 里的其他方法一样
+    ...mapActions(["incrementStep"])
   }
-}
+};
 </script>
